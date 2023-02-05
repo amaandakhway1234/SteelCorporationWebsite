8f145416d7b8090e98c5623146627213e57c5b9c
Amaan Dakhway <amaandakhway1234@gmail.com>
2023-02-05 17:07:44 +0530

    Created The Home Page

---
+++ asset/js/custom.js
@@ -0,0 +1,9 @@
+/* Theme Name:iDea - Clean & Powerful Bootstrap Theme
+ * Author:HtmlCoder
+ * Author URI:https://www.htmlcoder.me
+ * Author e-mail:htmlcoder.me@gmail.com
+ * Version: 1.5
+ * Created:October 2014
+ * License URI:https://support.wrapbootstrap.com/
+ * File Description: Place here your custom scripts
+ */
